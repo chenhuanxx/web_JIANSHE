06d54e27b15fb44e146503c3466a9b1fcab4c862
chen_xx <645036583@qq.com>
2019-11-08 15:23:15 +0800

    no message

---
+++ src/components/xyfc.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="center mar-t-2">
+				 
+					
+					<div class="zszx-left">
+						<ul>
+	        				<strong>招生咨询</strong>
+	        				<p>招生咨询qq : 
+	        					<a target="_blank" href="http://wpa.qq.com/msgrd?v=3&uin=1276520&site=qq&menu=yes" id="udesk-feedback-tab" title="欢迎您前来咨询">
+	        						1276520 <img border="0" src="../images/group.png" alt="加入QQ群" title="加入QQ群" width="65px">
+	        					</a>
+	        				</p>
+	        				<li>咨询电话1 :   0512-62933888</li>
+	        				<li>咨询电话2 :   0512-62932251</li>
+	        				<li>咨询电话3 :   0512-62932092</li>
+	        				<li>学校地址 : 江苏省苏州独墅湖科教创新区若水路99号</li>
+	        				
+	        			</ul>
+	        			<dl>
+							<dt> <img src="../images/wxewm.png" alt="" width="100px"></dt>
+							<dd>关注官方微信公众号</dd>
+						</dl>
+						<dl>
+							<dt><img src="../images/QQewm.png" alt="" width="100px"></dt>
+							<dd>官方咨询QQ群</dd>
+						</dl>
+						
+					</div> 
+	        	
+        			
+			<div class="fl list">
+				
+                <div class="list-tp">
+					<dl>
+						<dt><img src="../images/about1.png"/></dt>
+						<dd>校训</dd>
+					</dl>
+					<dl>
+						<dt><img src="../images/about2.png"/></dt>
+						<dd>校训</dd>
+					</dl>
+						<dl>
+						<dt><img src="../images/about1.png"/></dt>
+						<dd>校训</dd>
+					</dl>
+					<dl>
+						<dt><img src="../images/about2.png"/></dt>
+						<dd>校训</dd>
+					</dl>
+						<dl>
+						<dt><img src="../images/about1.png"/></dt>
+						<dd>校训</dd>
+					</dl>
+					<dl>
+						<dt><img src="../images/about2.png"/></dt>
+						<dd>校训</dd>
+					</dl>
+					
+					
+				</div>
+				<div class="page" id="test1"></div>
+			</div>
+		</div>
+		
+</template>
+<script>
+  export default {
+     data() {
+      return {
+       
+       
+
+      };
+    },
+ 
+    methods: {
+     
+    },
+ };
+   
+</script>
+ 
+
+<style>
+
+</style>
---
+++ src/components/zsjz.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="center mar-t-2">
+				 
+					
+					<div class="zszx-left">
+						<ul>
+	        				<strong>招生咨询</strong>
+	        				<p>招生咨询qq : 
+	        					<a target="_blank" href="http://wpa.qq.com/msgrd?v=3&uin=1276520&site=qq&menu=yes" id="udesk-feedback-tab" title="欢迎您前来咨询">
+	        						1276520 <img border="0" src="../images/group.png" alt="加入QQ群" title="加入QQ群" width="65px">
+	        					</a>
+	        				</p>
+	        				<li>咨询电话1 :   0512-62933888</li>
+	        				<li>咨询电话2 :   0512-62932251</li>
+	        				<li>咨询电话3 :   0512-62932092</li>
+	        				<li>学校地址 : 江苏省苏州独墅湖科教创新区若水路99号</li>
+	        				
+	        			</ul>
+	        			<dl>
+							<dt> <img src="../images/wxewm.png" alt="" width="100px"></dt>
+							<dd>关注官方微信公众号</dd>
+						</dl>
+						<dl>
+							<dt><img src="../images/QQewm.png" alt="" width="100px"></dt>
+							<dd>官方咨询QQ群</dd>
+						</dl>
+						
+					</div> 
+	        	
+        			
+			<div class="fl list">
+				<div class="list-1">
+					<dl>
+						<dt><img src="../images/school1.jpg" width="100%"/></dt>
+						<dd>招生简章</dd>
+					</dl>
+					<ul>
+						<li><a href="../views/detailsPage.html">2019年国家奖学金评审会</a>
+					    			<img src="../images/new.png" height="25px"/>
+					    			<img src="../images/hot.png" height="25px"/>
+					    			<span>2019-10-10</span>
+					    		</li>
+					    		<li><a href="">2019年国家奖学金评审会</a><span>2019-10-10</span></li>
+					    		<li><a href="">2019年国家奖学金评审会</a><span>2019-10-10</span></li>
+					    		<li>我校召开2019年国家奖学金评审会<span>2019-10-10</span></li>
+					    		<li><a href="">2019年国家奖学金评审会</a><span>2019-10-10</span></li>
+					    		<li>我校召开2019年国家奖学金评审会<span>2019-10-10</span></li>
+					    		<li><a href="">2019年国家奖学金评审会</a><span>2019-10-10</span></li>
+					    		<li>我校召开2019年国家奖学金评审会<span>2019-10-10</span></li>
+					    		<li><a href="">2019年国家奖学金评审会</a><span>2019-10-10</span></li>
+					    		<li>我校召开2019年国家奖学金评审会<span>2019-10-10</span></li>
+					    		<li>我校校企合作课题获中华职教社课题成果二等奖<span>2019-10-10</span></li>
+					    		<li>我校校企合作课题获中华职教社课题成果二等奖<span>2019-10-10</span></li>
+					</ul>
+					
+					
+				</div>
+				<div class="page" id="test1"></div>
+			</div>
+		</div>
+		
+</template>
+<script>
+  export default {
+     data() {
+      return {
+       
+       
+
+      };
+    },
+ 
+    methods: {
+     
+    },
+ };
+   
+</script>
+ 
+
+<style>
+
+</style>
