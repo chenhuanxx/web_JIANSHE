d829993430f4df4b74fa463fa735224a1ca46fa7
chen_xx <645036583@qq.com>
2019-11-11 20:25:39 +0800

    no message

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,6 +3,5 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
-  // API_ROOT: '"http://192.168.2.40:8080"', //配置http请求头
+  NODE_ENV: '"development"', 
 })
