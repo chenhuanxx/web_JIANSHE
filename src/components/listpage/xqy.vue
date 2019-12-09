9306d01f7cbcda1c5828e85b6708fc8e32881968
chen_xx <645036583@qq.com>
2019-12-09 19:26:53 +0800

    no message

--- src/components/listpage/xqy.vue
+++ src/components/listpage/xqy.vue
@@ -1,32 +1,9 @@
 <template>
     <div class="center mar-t-2">
-				<div class="zszx-left">
-						<ul>
-	        				<strong>招生咨询</strong>
-	        				<p>招生咨询qq : 
-	        					<a target="_blank" href="http://wpa.qq.com/msgrd?v=3&uin=1276520&site=qq&menu=yes" id="udesk-feedback-tab" title="欢迎您前来咨询">
-	        						1276520 <img border="0" src="../../images/group.png" alt="加入QQ群" title="加入QQ群" width="65px">
-	        					</a>
-	        				</p>
-	        				<li>咨询电话1 :   0512-62933888</li>
-	        				<li>咨询电话2 :   0512-62932251</li>
-	        				<li>咨询电话3 :   0512-62932092</li>
-	        				<li>学校地址 : 江苏省苏州独墅湖科教创新区若水路99号</li>
-	        				
-	        			</ul>
-	        			<dl>
-							<dt> <img src="../../images/wxewm.png" alt="" width="100px"></dt>
-							<dd>关注官方微信公众号</dd>
-						</dl>
-						<dl>
-							<dt><img src="../../images/QQewm.png" alt="" width="100px"></dt>
-							<dd>官方咨询QQ群</dd>
-						</dl>
-						
-					</div>
-        			 
-			<div class="fl list">
-				<div class="list-page"> 
+					<app-Left></app-Left>
+
+			<div class="fl list" style="position:relative;">
+				<div class="list-page">
 					<dl>
 						<dt>{{title}}</dt>
 						<dd>发布时间：{{updateDate}}</dd>
@@ -34,52 +11,58 @@
 					<div class="list-page-nr" v-html="content">
 						{{content}}
 					</div>
+					 <ul style="position:absolute;bottom:10px">
+						<li v-for="(item , index) in fileList" :key="index"  >附件：<a :href="item.url" style="color:#0893b0" >{{item.name}}</a> </li>
+					</ul>
+         
 				</div>
+				
+
 			</div>
 
 		</div>
 </template>
 <script>
-export default {
+import Left from '../left/Left.vue';
+ 
+  export default {
+	  components:{
+			"app-Left":Left,
+		},
 	data(){
 		return{
 			id:'',
-			// type:'',
 			updateDate:'',
 			content:'',
-			title:'', 
-			// clickTimes:'',
-			
+			title:'',
+			host:'http://47.92.84.126',
+			fileList:[],
 		}
 	},
 	created(){
-		// POST /app/article/info/{id}
 		this.id=this.$route.query.id;
-		// this.typeid=this.$route.query.type;
-		// console.log(this.id)
-		// console.log(this.type)
-		this.getdata();
-		
+		this.getdata(); 
+
 	},
 	methods:{
 		getdata(){
-				// let list={
-				// 	// type:'articleEnrollPlaning',
-				// 	id:this.id
-				// } 
 				this.$http.post("/app/article/info/"+this.id).then((response) => {
 					this.updateDate=response.data.article.updateDate;
 					this.content=response.data.article.content;
 					this.title=response.data.article.title;
 					// this.clickTimes=response.data.clickTimes;
-					 
-					
-					console.log(response.data.article)
-					 
-				}) 
+					this.fileList =response.data.article.fileList;
+					for(var i =0;i<this.fileList.length;i++){
+						this.fileList[i].url=this.host + this.fileList[i].url;
+					}  
+				})
 
 			},
+			// dom(index){
+			// 	console.log(index)
+			// 	window.location.href = "http://47.92.84.126/filedata/\doc\8f53cc6e-36f7-4aa1-9a87-946de73524d5.xlsx"
+			// }
 
 	},
 }
-</script>
+</script> 
--- src/router/index.js
+++ src/router/index.js
@@ -5,17 +5,41 @@ Vue.use(Router)
 
   
 export default new Router({
-  mode: 'history',
+  // mode: 'history',
   routes: [
     {path:'/',name:'首页',component: () => import('@/components/Home.vue'),},                         // 斜杠代表根路径  ”component:Home“ 表示点击 "主页" 的时候会展示Home组件的内容
-    {path:'/tzgg',name:'通知公共',component: () => import('@/components/tzgg.vue'),},
-    {path:'/zyjs',name:'专业介绍',component: () => import('@/components/zyjs.vue'),},
-    {path:'/zsjz',name:'招生简章',component: () => import('@/components/zsjz.vue'),},  
-    {path:'/zsjh',name:'招生计划',component: () => import('@/components/zsjh.vue'),},     
-    {path:'/lqcx',name:'录取查询',component: () => import('@/components/lqcx.vue'),},
-    {path:'/xyfc',name:'校园风采',component: () => import('@/components/xyfc.vue'),},
-    {path:'/bdzn',name:'报道指南',component: () => import('@/components/bdzn.vue'),},
-    {path:'/lxwm',name:'联系我们',component: () => import('@/components/lxwm.vue'),},
+    {path:'/tzgg',name:'通知公共',component: () => import('@/components/tzgg.vue'), meta:{
+      title: '通知公共',
+       
+    }},
+    {path:'/zyjs',name:'学院介绍',component: () => import('@/components/zyjs.vue'),meta:{
+      title: '学院介绍',
+       
+    }},
+    {path:'/zsjz',name:'招生简章',component: () => import('@/components/zsjz.vue'),meta:{
+      title: '招生简章',
+       
+    }},  
+    {path:'/zsjh',name:'招生计划',component: () => import('@/components/zsjh.vue'),meta:{
+      title: '招生计划',
+       
+    }},       
+    {path:'/lqcx',name:'录取查询',component: () => import('@/components/lqcx.vue'),meta:{
+      title: '录取查询',
+      keepAlive: false
+    }},  
+    {path:'/xyfc',name:'校园风采',component: () => import('@/components/xyfc.vue'),meta:{
+      title: '校园风采',
+      keepAlive: false
+    }},  
+    {path:'/bdzn',name:'报道指南',component: () => import('@/components/bdzn.vue'),meta:{
+      title: '报道指南',
+      keepAlive: false
+    }},  
+    {path:'/lxwm',name:'联系我们',component: () => import('@/components/lxwm.vue'),meta:{
+      title: '联系我们',
+      keepAlive: false
+    }},  
 
 
     {path:'/listpage/xqy',name:'/listpage/xqy',component: () => import('@/components/listpage/xqy.vue'),},
